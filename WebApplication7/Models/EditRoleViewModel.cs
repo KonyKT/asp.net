9a79b023260242a684edd4577b9e3a4a09cca3d3
KonyKT <konradtoczydlowsky@gmail.com>
2020-01-14 00:21:40 +0100

    ye

---
+++ WebApplication7/Models/EditRoleViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication7.Models
+{
+    public class EditRoleViewModel
+    {
+        public EditRoleViewModel()
+        {
+            Users = new List<String>();
+        }
+        public String Id { get; set; }
+        [Required(ErrorMessage = "Musi byc nazwa")]
+        public string RoleName { get; set; }
+
+        public List<String> Users {get; set;}
+    }
+}
